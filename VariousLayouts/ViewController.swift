98303432055adf561e403be3755f33fcee44ca8e
Red <seryprk@gmail.com>
2023-06-02 12:43:55 +0900

    START: start project

---
+++ VariousLayouts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VariousLayouts
+//
+//  Created by 박세리 on 2023/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
